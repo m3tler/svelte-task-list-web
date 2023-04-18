5b4c94a1e9e2aeac74797ab92732a28e8acb5a49
m3tler <dawid.metlerski@outlook.com>
2023-04-18 13:26:39 +0200

    Make some changes

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -12,5 +12,8 @@ export const handle: Handle = async ({ event, resolve }) => {
 		}
 	}
 	const response = await resolve(event);
+	response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate');
+	response.headers.set('Expires', '0');
+	response.headers.set('Pragma', 'no-cache');
 	return response;
 };
--- src/routes/(auth)/login/+page.server.ts
+++ src/routes/(auth)/login/+page.server.ts
@@ -1,6 +1,5 @@
 import { PUBLIC_API_URL } from '$env/static/public';
 import { fail, type Actions, redirect } from '@sveltejs/kit';
-import { setContext } from 'svelte';
 
 export const actions = {
 	login: async ({ cookies, request }) => {
